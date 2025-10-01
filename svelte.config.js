a0c58104a037c1da5486f3cd306df3ed338d9ef5
Rahat Ali <rahatchacho@hotmail.com>
2025-10-02 02:24:39 +0500

    Added production build

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/chacho-gpt-frontend' : ''
-		}
+		})
 	}
 };
 
