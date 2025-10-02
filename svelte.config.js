5be254a67da4481896d2d8693585d35d7bb633ae
Rahat Ali <rahatchacho@hotmail.com>
2025-10-02 16:30:05 +0500

    Changed output to public directory

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'public',      // ye change karo
+			assets: 'public',     // ye change karo
 			fallback: 'index.html'
 		})
 	}
